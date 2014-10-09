be56b722ba7eb9087692a6c9ef2ce6bca6d5bcf3
Mark Miyashita <negativetwelve@gmail.com>
2014-10-08 23:09:20 -0700

    Add remember_token and digest to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token
+
+  class << self
+    def new_remember_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def digest(token)
+      Digest::SHA1.hexdigest(token.to_s)
+    end
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.digest(User.new_remember_token)
+  end
+
 end
