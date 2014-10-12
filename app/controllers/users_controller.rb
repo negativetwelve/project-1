6aceb977cb9439196401b5728998e66c456e14d8
Howard Chen <howardxchen@gmail.com>
2014-10-11 23:41:00 -0700

    Link sign up to show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
+  def show
+    @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to @user
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
 # END
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'users/new'
+  get 'users/:id', to: 'users#show', as: :user
 
   root to: "pages#home"
 
